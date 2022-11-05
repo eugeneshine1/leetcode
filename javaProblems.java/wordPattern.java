28578654035e9e886ea4dfeafcc90e68e1ed4ae8
Eugene Shin <eugeneshine1@gmail.com>
2022-11-05 12:19:04 -0400

    Update wordPattern.java
    instantiate map
    loop through array and if indexes of pattern and words not match then return false

--- javaProblems.java/wordPattern.java
+++ javaProblems.java/wordPattern.java
@@ -3,5 +3,10 @@ class Solution {
         String[] words = s.split(" ");
         if (words.length != pattern.length())
             return false;
+        Map map = new HashMap();
+    for (Integer i=0; i<words.length; ++i)
+        if (map.put(pattern.charAt(i), i) != map.put(words[i], i))
+            return false;
+    return true;
     }
 }
