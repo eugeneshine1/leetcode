7c01b196ff4a15d0b346bd35dffa7fd87a2829a0
Eugene Shin <eugeneshine1@gmail.com>
2022-11-07 11:19:28 -0500

    while loop for reversed num

--- javaProblems.java/palindromeNumber.java
+++ javaProblems.java/palindromeNumber.java
@@ -6,6 +6,20 @@ class Solution {
         // compare if the new int is same as x 
         // return true if it is, else return false
         
-        
+        if (x<0) {
+            return false;
+        }
+        int palin=0;
+        int originalNum = x;
+        while(x>0) {
+            int lastDigit = x %10;
+            palin = palin*10 + lastDigit;
+            x=x/10;
+        }
+        if (palin==originalNum) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
