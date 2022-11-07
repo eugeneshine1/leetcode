172ab6f74e70e2c5de51324266a03666e246adfc
Eugene Shin <eugeneshine1@gmail.com>
2022-11-07 14:12:01 -0500

    pseudo code

---
+++ javaProblems.java/romanToInteger.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int romanToInt(String s) {
+        // loop through each character in the string 
+        // add each value of the symbol to the answer 
+        // if statements for I, X, C when they are before V, C, M respectively, minus                value instead
+        // return answer in int
+       
+    }
+}
