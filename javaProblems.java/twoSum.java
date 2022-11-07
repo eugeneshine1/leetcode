870e5dcb6ef03115aa632e0157e338a9f2dd22ae
Eugene Shin <eugeneshine1@gmail.com>
2022-11-07 10:40:04 -0500

    double for loop

--- javaProblems.java/twoSum.java
+++ javaProblems.java/twoSum.java
@@ -2,6 +2,15 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         // double loop through the array and find each combination
         //if the combination adds up to target return x,y indexes 
-        // indexes cannot be the same
+        // indexes cannot be the same int [] ans = new int [2];
+        for(int i=0; i<nums.length; i++) {
+            for(int idx =0; idx<nums.length; idx++) {
+                if(nums[i] + nums [idx] == target && i!=idx) {
+                    ans[0]=i;
+                    ans[1]=idx;
+                }
+            }
+        }
+        return ans;
     }
 }
