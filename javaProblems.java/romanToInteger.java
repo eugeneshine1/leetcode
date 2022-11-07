d85c254d7ba7907657a744b5c754f012142c1469
Eugene Shin <eugeneshine1@gmail.com>
2022-11-07 14:43:23 -0500

    double if statements for edge cases

--- javaProblems.java/romanToInteger.java
+++ javaProblems.java/romanToInteger.java
@@ -5,17 +5,29 @@ class Solution {
         // double if statements for I, X, C when they are before V, C, M respectively, minus value instead
         // return answer in int
         int ans =0; 
-        for (int i=s.length-1; i>=0; i--) {
+        for (int i=s.length()-1; i>=0; i--) {
             if (s.charAt(i)=='I') {
-                ans+=1;
+                if (i+1<=s.length()-1 && (s.charAt(i+1)=='V' || s.charAt(i+1)=='X')) {
+                    ans-=1; 
+                } else {
+                    ans+=1;
+                }
             } else if (s.charAt(i)=='V') {
                 ans+=5;
             } else if (s.charAt(i)=='X') {
-                ans+=10;
+               if (i+1<=s.length()-1 && (s.charAt(i+1)=='C' || s.charAt(i+1)=='L')) {
+                    ans-=10; 
+                } else {
+                    ans+=10;
+                }
             } else if (s.charAt(i)=='L') {
                 ans+=50;
             } else if (s.charAt(i)=='C') {
-                ans+=100;
+                if (i+1<=s.length()-1 && (s.charAt(i+1)=='M' ||  s.charAt(i+1)=='D')) {
+                    ans-=100; 
+                } else {
+                    ans+=100;
+                }
             } else if (s.charAt(i)=='D') {
                 ans+=500;
             } else if (s.charAt(i)=='M') {
