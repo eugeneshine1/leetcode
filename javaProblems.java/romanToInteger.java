5e22bc80257f0482f6dcaaface309f821a28cd03
Eugene Shin <eugeneshine1@gmail.com>
2022-11-07 14:18:34 -0500

    add value of symbols

--- javaProblems.java/romanToInteger.java
+++ javaProblems.java/romanToInteger.java
@@ -2,8 +2,26 @@ class Solution {
     public int romanToInt(String s) {
         // loop through each character in the string 
         // add each value of the symbol to the answer 
-        // if statements for I, X, C when they are before V, C, M respectively, minus                value instead
+        // double if statements for I, X, C when they are before V, C, M respectively, minus value instead
         // return answer in int
-       
+        int ans =0; 
+        for (int i=s.length-1; i>=0; i--) {
+            if (s.charAt(i)=='I') {
+                ans+=1;
+            } else if (s.charAt(i)=='V') {
+                ans+=5;
+            } else if (s.charAt(i)=='X') {
+                ans+=10;
+            } else if (s.charAt(i)=='L') {
+                ans+=50;
+            } else if (s.charAt(i)=='C') {
+                ans+=100;
+            } else if (s.charAt(i)=='D') {
+                ans+=500;
+            } else if (s.charAt(i)=='M') {
+                ans+=1000;
+            }
+        }
+        return ans;
     }
 }
