47a0d4adad8e270b85e389e614f67dd719a53112
Eugene Shin <eugeneshine1@gmail.com>
2022-11-07 10:38:50 -0500

    pseudo code

---
+++ javaProblems.java/twoSum.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // double loop through the array and find each combination
+        //if the combination adds up to target return x,y indexes 
+        // indexes cannot be the same
+    }
+}
