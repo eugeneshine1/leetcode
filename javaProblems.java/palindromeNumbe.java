a5d3ccd448a5118714c6de6d222fb4e21e297aa3
Eugene Shin <eugeneshine1@gmail.com>
2022-11-07 10:49:01 -0500

    pseudo code

---
+++ javaProblems.java/palindromeNumbe.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // if x is negative return false
+        // take the last digit and put it in the beginning of a new int
+        // keep doing this until the first number is added to the back of the new int
+        // compare if the new int is same as x 
+        // return true if it is, else return false
+        
+        
+    }
+}
