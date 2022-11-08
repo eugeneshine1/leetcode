b65764063eb714c07dd7dace4cbfda86be785ded
Eugene Shin <eugeneshine1@gmail.com>
2022-11-08 11:30:26 -0500

    pseudo code

---
+++ javaProblems.java/searchInsertPosition.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // loop through the array
+        // if the target number is between the number of x index and number of x-1 index, return the index x
+        // if the target is equal to number at index x or x-1 return x or x-1, respectively
+        // if the target is greater than the highest number, return highest index +1
+        // if the target is less than the lowest number, return 0
+    }
+}
