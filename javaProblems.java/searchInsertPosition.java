b0a351e1a4be220a4c84564c6906fe0260ef5f04
Eugene Shin <eugeneshine1@gmail.com>
2022-11-08 11:31:24 -0500

    for loop to search index

--- javaProblems.java/searchInsertPosition.java
+++ javaProblems.java/searchInsertPosition.java
@@ -5,5 +5,20 @@ class Solution {
         // if the target is equal to number at index x or x-1 return x or x-1, respectively
         // if the target is greater than the highest number, return highest index +1
         // if the target is less than the lowest number, return 0
+        for(int x=1; x<nums.length;x++){ 
+           
+              if(target>nums[x-1] && target<nums[x]){
+                 return x;
+             } else if (nums[x-1]==target) {
+                 return x-1;
+             }else if (nums[x]==target) {
+                 return x;
+             } else if (target>nums[nums.length-1]) {
+                 return nums.length;
+             }else if (target<nums[0]) {
+                 return 0;
+             }
+     }
+                 return nums.length;
     }
 }
