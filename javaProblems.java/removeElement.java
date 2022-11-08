197fd0745adbfdbca0588ea56ad0854a80c9d4ea
Eugene Shin <eugeneshine1@gmail.com>
2022-11-08 11:16:25 -0500

    pseudo code

---
+++ javaProblems.java/removeElement.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        //loop through the array
+        //if the number is not equal to the value to be removed, set it to the beginnging of the array
+        // return the count of numbers that arent the value to be removed
+    }
+}
