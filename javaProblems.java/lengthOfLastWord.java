9e1ad1592e2972eefc8caa1d79df16675edb910c
Eugene Shin <eugeneshine1@gmail.com>
2022-11-08 13:23:30 -0500

    loop to count

--- javaProblems.java/lengthOfLastWord.java
+++ javaProblems.java/lengthOfLastWord.java
@@ -3,5 +3,13 @@ class Solution {
         // split the string into string array
         // count the characters of the last string
         //return the count
+          
+        String [] strArr = s.split(" ");
+        int count=0; 
+        String lastWord = strArr[strArr.length -1];
+        for (int x=0; x<lastWord.length();x++) {
+            count++;
+        }
+        return count;
     }
 }
