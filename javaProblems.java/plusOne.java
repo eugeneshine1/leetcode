bf9e2fbc2b5012b6fd4b9d4943c357bc8e39b1a9
Eugene Shin <eugeneshine1@gmail.com>
2022-11-09 15:32:00 -0500

    pseudo code

---
+++ javaProblems.java/plusOne.java
@@ -0,0 +1,10 @@
+public class plusOne {
+      // loop through the array backwards and if the number is less than nine, add one to the value of the number and return the array 
+        // if number at any time is 9, set it to zero and continue the loop
+        
+        // for when the value of the beginning of the array is 9
+        // instantiate a new array with size 1 larger than previous array
+        // make the first number equal to 1
+        // return the new array (with rest of the values 0)
+
+}
