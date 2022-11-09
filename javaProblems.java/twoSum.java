1fef1d762c604c8087f2355dab69ef3aa7806e31
Eugene Shin <eugeneshine1@gmail.com>
2022-11-09 15:07:34 -0500

    instantiate new array

--- javaProblems.java/twoSum.java
+++ javaProblems.java/twoSum.java
@@ -3,6 +3,7 @@ class Solution {
         // double loop through the array and find each combination
         //if the combination adds up to target return x,y indexes 
         // indexes cannot be the same int [] ans = new int [2];
+        int [] ans = new int [2];
         for(int i=0; i<nums.length; i++) {
             for(int idx =0; idx<nums.length; idx++) {
                 if(nums[i] + nums [idx] == target && i!=idx) {
