6130ccad19bc0fb1ac8c8d8bfaed0e27afc17778
Eugene Shin <eugeneshine1@gmail.com>
2022-11-10 15:32:48 -0500

    pseudo code

---
+++ javaProblems.java/validAnagram.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // make both strings into separate character arrays
+        // sort both the arrays and set it to equal each other (.equals not ==)
+        // .equals looks at value and == looks at reference 
+        // return if true, else false;
+    }
+}
