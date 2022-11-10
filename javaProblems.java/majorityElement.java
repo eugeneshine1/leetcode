fb4dc7b4ab6b0f588f3a36b2e48c59d67f931efd
Eugene Shin <eugeneshine1@gmail.com>
2022-11-10 14:58:53 -0500

    return middle element in sorted array
    if majority, the middle element should be the value that is most common in the array

---
+++ javaProblems.java/majorityElement.java
@@ -0,0 +1,9 @@
+public class majorityElement {
+    class Solution {
+        public int majorityElement(int[] nums) {
+            // sort array and return that middle element 
+            Arrays.sort(nums);
+        return nums[(nums.length-1)/2];
+        }
+    }
+}
