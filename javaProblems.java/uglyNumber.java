7844978aad280d8dd1ecb1f61034bdbec8efd114
Eugene Shin <eugeneshine1@gmail.com>
2022-11-11 11:32:12 -0500

    pseudo code

---
+++ javaProblems.java/uglyNumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isUgly(int n) {
+        // if the number is less than 1 return false
+        // while loop through when the number is still divisble by 2,3,5
+        // if it is divisble by 2, divide the number by 2
+        // do this step if it is divisble by 2,3,5
+        // if the number ends up being one, return true
+        // else return false
+    }
+}
