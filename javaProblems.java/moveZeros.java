3cc4fd4d7e9df2cafca4cc412b1e9c1abe2a36a0
Eugene Shin <eugeneshine1@gmail.com>
2022-11-14 15:59:33 -0500

    double for loop

--- javaProblems.java/moveZeros.java
+++ javaProblems.java/moveZeros.java
@@ -1,6 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         // use a bubble sort and make all zeros to right side
-        
+        for (int i = 0; i<nums.length; i++) {
+            for (int idx =0; idx<nums.length; idx ++) {
+                if (nums[idx]==0) {
+                    int temp = nums[idx];
+                    nums[idx]= nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
     }
 }
