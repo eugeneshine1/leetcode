a79e17fcd9a0d71ed589ced711e9dea78bcfbc01
Eugene Shin <eugeneshine1@gmail.com>
2022-11-14 16:05:36 -0500

    for loop to add nonzeros and while loop to add zeros to the end

--- javaProblems.java/moveZeros.java
+++ javaProblems.java/moveZeros.java
@@ -2,5 +2,16 @@ class Solution {
     public void moveZeroes(int[] nums) {
        // loop though and set each non zero number to the beginning of the array
        // do another loop to add the rest of the zeros into the array starting where the count left off from
+       int count = 0;  
+ 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0)
+                nums[count++] = nums[i]; 
+        }
+      
+        while (count < nums.length) {
+            nums[count++] = 0;
+    
+        } 
     }
 }
