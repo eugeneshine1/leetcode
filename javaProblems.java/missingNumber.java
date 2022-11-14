0580e15c79597e86b3eaff47847296fe588ef6a8
Eugene Shin <eugeneshine1@gmail.com>
2022-11-14 15:33:52 -0500

    pseudo code

---
+++ javaProblems.java/missingNumber.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // add the numbers from array size to 1
+        // subtract the value in the array from the sum
+        // return the remaining value of sum, which should be the missing number
+    }
+}
