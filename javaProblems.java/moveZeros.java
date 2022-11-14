c1d5b8165a8586704feec40eba1c4ce7681d3839
Eugene Shin <eugeneshine1@gmail.com>
2022-11-14 16:04:02 -0500

    new pseudo code

--- javaProblems.java/moveZeros.java
+++ javaProblems.java/moveZeros.java
@@ -1,14 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // use a bubble sort and make all zeros to right side
-        for (int i = 0; i<nums.length; i++) {
-            for (int idx =0; idx<nums.length; idx ++) {
-                if (nums[idx]==0) {
-                    int temp = nums[idx];
-                    nums[idx]= nums[i];
-                    nums[i] = temp;
-                }
-            }
-        }
+       // loop though and set each non zero number to the beginning of the array
+       // do another loop to add the rest of the zeros into the array starting where the count left off from
     }
 }
