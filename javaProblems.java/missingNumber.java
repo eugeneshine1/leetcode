cfff86ba5dfe309df2cb03c16bbd17c600a78169
Eugene Shin <eugeneshine1@gmail.com>
2022-11-14 15:36:56 -0500

    2 for loops

--- javaProblems.java/missingNumber.java
+++ javaProblems.java/missingNumber.java
@@ -3,5 +3,15 @@ class Solution {
         // add the numbers from array size to 1
         // subtract the value in the array from the sum
         // return the remaining value of sum, which should be the missing number
+        
+        int sum=0; 
+        for (int i=1 ; i<=nums.length;i++) {
+            sum += i;
+        }
+        int missingNum =sum ;
+         for (int i=0 ; i<nums.length;i++) {
+            missingNum -= nums[i];
+        }
+        return missingNum;
     }
 }
