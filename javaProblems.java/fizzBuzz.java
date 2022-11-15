44b9cec3c7ace3285b5d79dd0e2bb9c39e3cef8f
Eugene Shin <eugeneshine1@gmail.com>
2022-11-15 13:40:20 -0500

    iterate through for loop

--- javaProblems.java/fizzBuzz.java
+++ javaProblems.java/fizzBuzz.java
@@ -7,6 +7,18 @@ class Solution {
         // if the number is divisble by 3, add "Fizz" to list
         // if non of the above, add the number to the list as string
         // return list
-
+        List <String> list = new ArrayList<>();
+        for (int i=1; i<=n;i++) {
+            if (i%15 ==0) {
+                list.add("FizzBuzz");
+            } else if (i%5 ==0) {
+                list.add("Buzz");
+            } else if (i%3 ==0) {
+                list.add("Fizz");
+            } else {
+                list.add(Integer.toString(i));
+            }
+        }
+        return list;
     }
 }
