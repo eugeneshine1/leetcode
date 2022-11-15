bbab21c4948b12c791d75633b842c4943bf434b0
Eugene Shin <eugeneshine1@gmail.com>
2022-11-15 13:12:55 -0500

    pseudo code

---
+++ javaProblems.java/reverseString.java
@@ -0,0 +1,7 @@
+class Solution {
+    public void reverseString(char[] s) {
+        // set variables to the first and last index of the char array
+        // while loop where you switch the first char position with the last char position 
+        // continue the loop by switching with the second and second to last, so on and so forth
+    }    
+}
