5e35cd6e6f9c8a9d21839e83551b06db372ba689
Eugene Shin <eugeneshine1@gmail.com>
2022-11-15 13:26:26 -0500

    pseudo code

---
+++ javaProblems.java/firstUniqueChar.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        // turn the string into a char array 
+        // for each loop to check every character in the array
+        // if the index of the a character matches the last index of a character, then set the index to a variable
+        // break the loop so it just looks for the first unique character
+        // else return -1 because there are no unique characters
+        // return the index of the character
+        
+    }
+}
