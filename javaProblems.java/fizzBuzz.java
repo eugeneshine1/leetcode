f5d239174b16a3e02f94e1c7dbfd00189b81d036
Eugene Shin <eugeneshine1@gmail.com>
2022-11-15 13:37:41 -0500

    pseudo code

---
+++ javaProblems.java/fizzBuzz.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        // create an arraylist that will contain strings
+        // use for loop and interate from 1 to given number n
+        // if the number is divisible by 15 or both 5 and 3, add "FizzBuzz" to list
+        // if the number is divisble by 5, add "Buzz" to list
+        // if the number is divisble by 3, add "Fizz" to list
+        // if non of the above, add the number to the list as string
+        // return list
+
+    }
+}
