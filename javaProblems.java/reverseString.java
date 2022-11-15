67e5c054d731a781c81edfb5d82f08406c6b3560
Eugene Shin <eugeneshine1@gmail.com>
2022-11-15 13:15:00 -0500

    while loop with sorting algorithm

--- javaProblems.java/reverseString.java
+++ javaProblems.java/reverseString.java
@@ -3,5 +3,16 @@ class Solution {
         // set variables to the first and last index of the char array
         // while loop where you switch the first char position with the last char position 
         // continue the loop by switching with the second and second to last, so on and so forth
+
+        int firstChar = 0;
+        int lastChar = s.length - 1;
+        
+        while(firstChar <= lastChar){
+            char temp = s[firstChar];
+            s[firstChar] = s[lastChar];
+            s[lastChar] = temp;
+            firstChar++;
+            lastChar--;
+        }
     }    
 }
