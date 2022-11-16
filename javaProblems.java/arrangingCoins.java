64d52990a6adb70516d95ac4e0db9ce1241ae707
Eugene Shin <eugeneshine1@gmail.com>
2022-11-16 13:59:04 -0500

    while loop until coins run out

--- javaProblems.java/arrangingCoins.java
+++ javaProblems.java/arrangingCoins.java
@@ -8,5 +8,16 @@ class Solution {
         // (because with each step, there needs to be one more step than previous)
         // return the number of steps-1 because we only want complete stairs
 
+        if (n==1){
+            return 1;
+        }
+        int steps=0; 
+        int max=1;
+        while (n>=0){;
+            steps++;
+            n = n - max;
+            max = max + 1;
+        }
+        return steps-1;
     }
 }
