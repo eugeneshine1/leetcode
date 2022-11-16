27c8e8da5f6d45e8cf3471e5a1549c836ce8302e
Eugene Shin <eugeneshine1@gmail.com>
2022-11-16 13:58:22 -0500

    pseudo code

---
+++ javaProblems.java/arrangingCoins.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        // if you only have one coin, return 1
+        // while loop until you run out of coins
+        // each loop increase the number of steps/staircase you would have
+        // subtract 1 coin from the total
+        // have a variable where the subtracted number increases by 1 
+        // (because with each step, there needs to be one more step than previous)
+        // return the number of steps-1 because we only want complete stairs
+
+    }
+}
