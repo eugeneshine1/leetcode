994232cbb430d72052039bbda21cf11e6d854dc4
Eugene Shin <eugeneshine1@gmail.com>
2022-11-17 10:29:57 -0500

    for loop to add to sum

--- javaProblems.java/perfectNumber.java
+++ javaProblems.java/perfectNumber.java
@@ -5,6 +5,18 @@ class Solution {
         // add each number to the sum variable if number is divisble by the index
         // set the sum and num equal to each other and if it matches return true
         // else return false
-        
+        int sum =0;
+            if (num%2 !=0) {
+                return false;
+            }
+        for (int i=1; i<=num/2;i++) {
+            if (num%i ==0){
+            sum+=i;
+            }
+        }
+        if (sum==num){
+            return true;
+        } 
+        return false;
     }
 }
