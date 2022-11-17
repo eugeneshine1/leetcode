685e126669ee8ce7335732c7e9470e6867bc1091
Eugene Shin <eugeneshine1@gmail.com>
2022-11-17 10:58:57 -0500

    for loop to count uppercase

--- javaProblems.java/dectectCapital.java
+++ javaProblems.java/dectectCapital.java
@@ -4,6 +4,18 @@ class Solution {
         //if there is no uppercase (count is 0), then return true
         //if the count is 1 and the uppercase is in the beginning, return true
         //return word length equals to count in case that all are capital letters
-
+        int count=0;
+        for (int i=0; i<word.length();i++) {
+            if(Character.isUpperCase(word.charAt(i))) {
+                count++;
+            }
+        }
+        if (count==0) {
+            return true;
+        }
+        if (count==1 && Character.isUpperCase(word.charAt(0))) {
+            return true;
+        }
+        return count==word.length();
     }
 }
