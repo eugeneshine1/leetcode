96e62d86d43b2ec9d8183679ee701e96568a9aad
Eugene Shin <eugeneshine1@gmail.com>
2022-11-17 10:28:00 -0500

    pseudo code

---
+++ javaProblems.java/perfectNumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        // if the number is odd return false
+        // loop through from 1 to half the number 
+        // add each number to the sum variable if number is divisble by the index
+        // set the sum and num equal to each other and if it matches return true
+        // else return false
+        
+    }
+}
