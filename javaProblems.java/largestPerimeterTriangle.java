38f0597b1400dd628d9372f4829dd2e97272f0e6
Eugene Shin <eugeneshine1@gmail.com>
2022-11-17 11:12:17 -0500

    arrays.sort and iterate through array

--- javaProblems.java/largestPerimeterTriangle.java
+++ javaProblems.java/largestPerimeterTriangle.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public int largestPerimeter(int[] nums) {
         // sort the array from least to greatest
@@ -5,6 +7,13 @@ class Solution {
         // if the last number is less than the sum of the other two int elements, return the sum of all 3 int elements
         //(as long as the 2 smaller sides together is bigger than the larger number, a triangle can be formed)
         // return 0 when it does not match the first return statement
-
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=n-1;i>1;i--) {
+            if(nums[i]<nums[i-1]+nums[i-2]) {
+                return nums[i]+nums[i-1]+nums[i-2];
+            }
+        }
+        return 0;
     }
 }
