c96c619975f11fff2e6eb1404b437221a81f2585
Eugene Shin <eugeneshine1@gmail.com>
2022-11-17 11:10:54 -0500

    pseudo code

---
+++ javaProblems.java/largestPerimeterTriangle.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        // sort the array from least to greatest
+        // loop from last number of array to first
+        // if the last number is less than the sum of the other two int elements, return the sum of all 3 int elements
+        //(as long as the 2 smaller sides together is bigger than the larger number, a triangle can be formed)
+        // return 0 when it does not match the first return statement
+
+    }
+}
