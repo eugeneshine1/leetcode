8caccf31fc770db55b1e9dbb40f747f9960f2f1c
Eugene Shin <eugeneshine1@gmail.com>
2022-11-17 10:57:58 -0500

    pseudo code

---
+++ javaProblems.java/dectectCapital.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        //loop through the string and if a char is uppercase, add 1 to the counter
+        //if there is no uppercase (count is 0), then return true
+        //if the count is 1 and the uppercase is in the beginning, return true
+        //return word length equals to count in case that all are capital letters
+
+    }
+}
