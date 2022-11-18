cdf3674c17cce12da6232d16852d9f350c198d63
Eugene Shin <eugeneshine1@gmail.com>
2022-11-18 11:02:53 -0500

    while loop for binary search algo

--- javaProblems.java/binarySearch.java
+++ javaProblems.java/binarySearch.java
@@ -8,5 +8,19 @@ class Solution {
         // if the value of the middle index is greater than the target value, set the new top to be the index of middle -1, so it will search only everything below it
         // if the value of the middle index is less than the target value, set the new bot to be the index of middle -1, so it will search only everything above it
         // if target value doesnt exist, return -1
+
+        int top = nums.length-1;
+        int bot =0;
+        while (bot<=top) {
+            int mid =(top+bot)/2; 
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid]>= target) {
+                top = mid -1;
+            } else {
+                bot = mid +1 ;
+            }
+        }
+        return -1;
     }
 }
