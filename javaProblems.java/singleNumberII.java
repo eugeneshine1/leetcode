726a44ee59ffedee1d38ac371f2e09884ea884e7
Eugene Shin <eugeneshine1@gmail.com>
2022-11-21 13:21:37 -0500

    revised pseudo code

--- javaProblems.java/singleNumberII.java
+++ javaProblems.java/singleNumberII.java
@@ -1,7 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         // create hashmap with key and value to be both integer
-        // key is the number in the array and value is the index of the array
-        // return the value of the when the value is equal to 1;
+        // key is the number in the array but no value
+        // loop through and add to hashmap
+        // use getOrDefault so that it puts 0 because there is no mapped value
+        // add +1 at the end, this allows a counting for keys that are the same
+        // another loop to look at the values(the count) of the map
+        // return the key of the when the value(count) is equal to 1;
     }
 }
