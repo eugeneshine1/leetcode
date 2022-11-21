e60de9f4a4fe9bb28c65e5d70741c37e52dab556
Eugene Shin <eugeneshine1@gmail.com>
2022-11-21 13:27:11 -0500

    hashmap to find count of numbers

--- javaProblems.java/singleNumberII.java
+++ javaProblems.java/singleNumberII.java
@@ -7,5 +7,16 @@ class Solution {
         // add +1 at the end, this allows a counting for keys that are the same
         // another loop to look at the values(the count) of the map
         // return the key of the when the value(count) is equal to 1;
+
+        Map <Integer, Integer> map  = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for (int i=0; i<nums.length; i++) {
+            if (map.get(nums[i])==1) {    
+                return nums[i];
+            }
+        }
+        return 0;
     }
 }
