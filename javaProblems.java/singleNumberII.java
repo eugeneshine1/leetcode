bfa59abebc0935cd960b18b326887790906a154b
Eugene Shin <eugeneshine1@gmail.com>
2022-11-21 13:17:45 -0500

    pseudo code

---
+++ javaProblems.java/singleNumberII.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        // create hashmap with key and value to be both integer
+        // key is the number in the array and value is the index of the array
+        // return the value of the when the value is equal to 1;
+    }
+}
