cb0840f2df1d8d2a2cbb3be38a749c6da2ec3fef
Eugene Shin <eugeneshine1@gmail.com>
2022-12-01 10:30:39 -0500

    pseudo code

---
+++ javaProblems.java/averageEvenDivisibleBySix.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int averageValue(int[] nums) {
+        //interate through the array and see if the number is divisible by 6 because it must be divisble by 3 and even number (by 2);
+        //if it is divisible by 6, then add 1 to the counter and add the number to the sum
+        //return the the sum of all numbers divisble by the count, which will give you the average value of even numbers that are divisible by 3
+    }
+}
