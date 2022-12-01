45c4c2a1b04b1f4ea633df817b112d188ae12013
Eugene Shin <eugeneshine1@gmail.com>
2022-12-01 10:31:55 -0500

    for loop with counter

--- javaProblems.java/averageEvenDivisibleBySix.java
+++ javaProblems.java/averageEvenDivisibleBySix.java
@@ -3,5 +3,14 @@ class Solution {
         //interate through the array and see if the number is divisible by 6 because it must be divisble by 3 and even number (by 2);
         //if it is divisible by 6, then add 1 to the counter and add the number to the sum
         //return the the sum of all numbers divisble by the count, which will give you the average value of even numbers that are divisible by 3
+        int count=0;
+        int sum=0;
+        for(int x=0; x<nums.length;x++) {
+            if (nums[x]%6 ==0) {
+                count++;
+                sum+=nums[x];
+            }
+        }
+        return sum/count;
     }
 }
