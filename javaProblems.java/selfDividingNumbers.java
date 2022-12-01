6a70f9b3843b3132aca14e3650f5a0d1babe153f
Eugene Shin <eugeneshine1@gmail.com>
2022-12-01 11:54:17 -0500

    revised pseudo code (added another method)

--- javaProblems.java/selfDividingNumbers.java
+++ javaProblems.java/selfDividingNumbers.java
@@ -2,6 +2,15 @@ class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {
         //interate through the loop and check if each digit of a number is divisble to the whole number
         //if it is divisble to the whole number and the digit is not zero add it to the list created
+        //(this is checked by the other method in the same class) 
         //return the list
+    }
+
+    public boolean eachDigit(int n) {
+        //take the number and get the end digit
+        //check to see if the number is not zero and is divisble by the digit
+        //if not return false
+        //do the same thing with all digits in the number by doing a loop 
+        //return true
     }
 }
