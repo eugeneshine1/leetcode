f508ec9c0c88f62d4969084f28ae8133fcd70ee3
Eugene Shin <eugeneshine1@gmail.com>
2022-12-01 11:48:18 -0500

    pseudo code

---
+++ javaProblems.java/selfDividingNumbers.java
@@ -0,0 +1,7 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        //interate through the loop and check if each digit of a number is divisble to the whole number
+        //if it is divisble to the whole number and the digit is not zero add it to the list created
+        //return the list
+    }
+}
