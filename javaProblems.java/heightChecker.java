eb4966595f577297369a1e669f3d672093ffb648
Eugene Shin <eugeneshine1@gmail.com>
2022-12-07 12:51:54 -0500

    pseudo code

---
+++ javaProblems.java/heightChecker.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        //create a new array with the same values of the given array but sorted
+        //compare each each number of the indices
+        //if the number at the same indices do not match, add 1 to the counter
+        //return the counter
+        
+    }
+}
