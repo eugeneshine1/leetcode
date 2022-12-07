e66f07a2bead2ef97bb6ef925f47b74e8a8e7292
Eugene Shin <eugeneshine1@gmail.com>
2022-12-07 12:54:24 -0500

    compare sorted array to the original array

--- javaProblems.java/heightChecker.java
+++ javaProblems.java/heightChecker.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public int heightChecker(int[] heights) {
         //create a new array with the same values of the given array but sorted
         //compare each each number of the indices
         //if the number at the same indices do not match, add 1 to the counter
         //return the counter
-        
+
+        int[] sortedArr = new int [heights.length];
+        for (int i=0; i<=heights.length-1; i++) {
+            sortedArr[i]=heights[i];
+        }
+        Arrays.sort(sortedArr);
+
+        int counter=0; 
+        for (int i=0; i<=heights.length-1; i++) {
+            if (sortedArr[i]!=heights[i]) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
